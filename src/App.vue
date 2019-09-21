220fce93c04feab44dff06bc2107ed1b676728b9
Isaac Ingles <isaac.ingles@gmail.com>
2019-09-21 10:59:03 -0600

    Got rid of HelloWorld and created header component

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,18 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Header />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Header,
   },
   data: () => ({
     //
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+     <v-row>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    
+  }),
+}
+</script>
