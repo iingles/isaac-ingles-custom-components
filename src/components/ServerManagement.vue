7603e44d10f4a94155d7dae2e577cf69321413f4
Isaac Ingles <isaac.ingles@gmail.com>
2019-09-29 11:55:48 -0600

    added styles to server cards

--- src/components/ServerManagement.vue
+++ src/components/ServerManagement.vue
@@ -3,11 +3,9 @@
         <v-col
             v-for="(server, serverKey) in servers" :key="serverKey"
         >
-            <v-card>
-                <v-card-title>
-                    {{ server.serverName }}
-                </v-card-title>
-                <p>Status:<span :class="server.serverStatus">{{ server.serverStatus }}</span></p>
+            <v-card class="server-info">
+                <v-card-title class="server-title">{{ server.serverName }}</v-card-title>
+                <p>Status:<span class="server-status" :class="server.serverStatus">{{ server.serverStatus }}</span></p>
                 <p>Message:<span class="server-message">{{ server.serverMsg }}</span></p>
             </v-card>
         </v-col>        
@@ -49,6 +47,9 @@ export default {
 </script>
 
 <style scoped>
+    .server-info {
+        padding: 10px;
+    }
     .Optimal {
         color: green;
     }
@@ -60,8 +61,19 @@ export default {
     .Error {
         color: red;
     }
-
+    .server-status {
+        background: #000;
+        padding: 10px;
+        font-size: 20px;
+        font-weight: bold
+    }
     .server-message {
         font-weight: bold;
+    }
+
+    .server-title {
+        background: navy;
+        color: #fff;
+        margin-bottom: 20px;
     }
 </style>
