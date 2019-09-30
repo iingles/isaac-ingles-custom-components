a0a33920863c65f029634ca953d35d2f8cb692d6
Isaac Ingles <isaac.ingles@gmail.com>
2019-09-30 11:19:06 -0600

    wrapped user image in v-img tags and centered

--- src/components/shared/CurrentUserDrawer.vue
+++ src/components/shared/CurrentUserDrawer.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="user-info">
         <p>Logged in as: <a href="#">Username</a></p>
-        <img src="http://placekitten.com/200/200">
+        <v-img 
+            class="img-center" 
+            src="http://placekitten.com/200/200"
+            max-width="200"
+            max-height="200"
+        ></v-img>
         <p><a href="#">Logout</a></p>
     </div>
 </template>
@@ -17,5 +22,8 @@ export default {
         margin: 10px 0 10px 0;
         padding: 10px;
         text-align: center;
+    }
+    .img-center {
+        margin: 0 auto;
     }
 </style>
