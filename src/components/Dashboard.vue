c2d0aafd8a8adaff7a0267c6f63d48b1384e8199
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-02 15:45:42 -0600

    playing with sparklines for dashboard statistics view

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>
+    <div class="card-row">
         <h1>Dashboard</h1>
         <v-row>
-            <v-card>
-                <v-card-title>Statistics</v-card-title>
-            </v-card>
+            <v-col lg="12" md="12" sm="12" xs="12">
+                <v-card >
+                    <v-card-title>Statistics</v-card-title>
+                    <v-container fluid>
+                        <v-sparkline
+                            :key="100"
+                            :smooth="16"
+                            :color="black"
+                            :line-width="1"
+                            :value=[25,2,15,77,35,4,55,25,43,55,34]
+                            height='75'
+                            auto-draw
+                            stroke-linecap="round"
+                        >
+                        </v-sparkline>
+                        <v-divider></v-divider>
+                        <v-sparkline
+                            :key="100"
+                            :smooth="16"
+                            :color="'#0000ff'"
+                            :line-width="1"
+                            :value=[55,75,4,1,35,11,25,22,77,20,103]
+                            height='75'
+                            auto-draw
+                            stroke-linecap="round"
+                        >
+                        </v-sparkline>
+                    </v-container>
+                </v-card>
+            </v-col>
         </v-row>
         <v-row>
             <v-card>
@@ -26,5 +53,8 @@ export default {
 </script>
 
 <style scoped>
+    .card-row {
+        width: 100%;
+    }
 
 </style>
