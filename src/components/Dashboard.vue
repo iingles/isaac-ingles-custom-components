a0fd1d4f9cfc1aa457840627e8e1826bb65dd594
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-02 16:00:23 -0600

    gave more styles to sparklines and dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,27 +4,30 @@
         <v-row>
             <v-col lg="12" md="12" sm="12" xs="12">
                 <v-card >
-                    <v-card-title>Statistics</v-card-title>
+                    <v-card-title class="card-title">Statistics</v-card-title>
                     <v-container fluid>
                         <v-sparkline
-                            :key="100"
+                            :key="25"
                             :smooth="16"
                             :color="black"
                             :line-width="1"
                             :value=[25,2,15,77,35,4,55,25,43,55,34]
-                            height='75'
+                            width="100"
+                            height="25"
                             auto-draw
                             stroke-linecap="round"
+                            
                         >
                         </v-sparkline>
                         <v-divider></v-divider>
                         <v-sparkline
-                            :key="100"
+                            :key="25"
                             :smooth="16"
                             :color="'#0000ff'"
                             :line-width="1"
-                            :value=[55,75,4,1,35,11,25,22,77,20,103]
-                            height='75'
+                            :value=[1,5,4,1,25,10,1,22,1,0,25,1,15,1,10,1,25,14,20,10,22,25]
+                            width="100"
+                            height='25'
                             auto-draw
                             stroke-linecap="round"
                         >
@@ -34,12 +37,12 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-card>
+            <v-card class="card-title">
                 <v-card-title>Alerts</v-card-title>
             </v-card>
         </v-row>
         <v-row>
-             <v-card>
+             <v-card class="card-title">
                 <v-card-title>Logs</v-card-title>
             </v-card>
         </v-row>
@@ -53,6 +56,10 @@ export default {
 </script>
 
 <style scoped>
+    .card-title {
+        background: navy;
+        color: #fff;
+    }
     .card-row {
         width: 100%;
     }
