b3194565620e98e4470e3b12492ad634c622849f
Isaac Ingles <isaac.ingles@gmail.com>
2019-10-04 14:21:38 -0600

    Fixed the layout for the helpdesk form

--- src/components/forms/HelpDesk.vue
+++ src/components/forms/HelpDesk.vue
@@ -3,15 +3,15 @@
         <h1>Contact the Help Desk</h1>
         <v-form>
             <v-row>
-                <v-row>
+                <v-col md="12" sm="12" xs="12">
                     <v-text-field
                     v-model.lazy="userData.firstname"
                     id="firstname"
                     label="First Name"
                     >
                     </v-text-field>
-                </v-row>
-                <v-row>
+                </v-col>
+                <v-col md="12" sm="12" xs="12">
                     <!-- .lazy doesn't read until after the next event; 
                     instead of on every keystroke -->
                     <v-text-field
@@ -20,21 +20,21 @@
                     label="Last Name"
                     >
                     </v-text-field>
-                </v-row>
-                <v-row>
+                </v-col>
+                <v-col md="12" sm="12" xs="12">
                     <v-text-field
                     v-model="userData.email"
                     id="email"
                     label="Email"
                     >
                     </v-text-field>
-                </v-row>
-                <v-row>
+                </v-col>
+                <v-col md="12" sm="12" xs="12">
                     <v-textarea
                     label="Please describe your issue"
                     >
                     </v-textarea>
-                </v-row>
+                </v-col>
             </v-row>
         </v-form>
     </v-container>    
--- src/components/shared/HeaderMenu.vue
+++ src/components/shared/HeaderMenu.vue
@@ -1,10 +1,10 @@
 <template>
     <v-list>
-        <v-list-item><v-icon>mdi-settings-outline</v-icon>Settings</v-list-item>
-        <v-list-item><v-icon>mdi-clock-outline</v-icon>History</v-list-item>
-        <v-list-item><v-icon>mdi-history</v-icon>Check for Updates</v-list-item>
+        <v-list-item to="#"><v-icon>mdi-settings-outline</v-icon>Settings</v-list-item>
+        <v-list-item to="#"><v-icon>mdi-clock-outline</v-icon>History</v-list-item>
+        <v-list-item to="#"><v-icon>mdi-history</v-icon>Check for Updates</v-list-item>
         <v-list-item to="/helpform"><v-icon>mdi-help</v-icon>Help</v-list-item>
-        <v-list-item><v-icon>mdi-information-outline</v-icon>About</v-list-item>
+        <v-list-item to="#"><v-icon>mdi-information-outline</v-icon>About</v-list-item>
     </v-list>
 </template>
 
